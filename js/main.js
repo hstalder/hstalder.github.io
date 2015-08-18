641779abe550b2fc052e243e54386f83b1da33c3
Hugo Stalder <hugo.stalder@sunrise.ch>
2015-08-18 15:11:22 +0200

    Remove twitter feed and socialize

--- js/main.js
+++ js/main.js
@@ -217,41 +217,6 @@ jQuery(function ($) {
 
 
 	/* ==================================================
-	 Twitter Feed
-	 ================================================== */
-
-	BRUSHED.tweetFeed = function () {
-
-		var valueTop = -64; // Margin Top Value
-
-		$("#ticker").tweet({
-			modpath: 'js/twitter/',
-			username: "Bluxart", // Change this with YOUR ID
-			page: 1,
-			avatar_size: 0,
-			count: 10,
-			template: "{text}{time}",
-			filter: function (t) {
-				return !/^@\w+/.test(t.tweet_raw_text);
-			},
-			loading_text: "loading ..."
-		}).bind("loaded", function () {
-			var ul = $(this).find(".tweet_list");
-			var ticker = function () {
-				setTimeout(function () {
-					ul.find('li:first').animate({marginTop: valueTop + 'px'}, 500, 'linear', function () {
-						$(this).detach().appendTo(ul).removeAttr('style');
-					});
-					ticker();
-				}, 5000);
-			};
-			ticker();
-		});
-
-	};
-
-
-	/* ==================================================
 	 Menu Highlight
 	 ================================================== */
 
